3ef356fe0e47220a6b0ac5abb3cde2347f47463f
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2023-08-05 21:22:24 -0300

    feat: migrated custom hooks

---
+++ data/constants.ts
@@ -0,0 +1,45 @@
+export const workExperience = [
+  {
+    position: "Full Stack Developer - @Ualá",
+    web: "https://www.uala.com.ar/",
+    fromDate: "2022",
+    toDate: "Actualidad",
+    details: [
+      {
+        functionStack: "Frontend",
+        detail:
+          "Desarrollos con NextJs, React Js, Tailwind CSS y Typescript. Además trabajo en una librería de componente con Storybook.",
+      },
+      {
+        functionStack: "Backend",
+        detail:
+          "Utilizando tecnologías como Golang y AWS, además integro infraestructura como código con Terraform y Terragrunt.",
+      },
+      {
+        functionStack: "Backend Mulesoft",
+        detail:
+          "Integraciones de servicios y creación de APIs mediante las herramientas de Mulesoft.",
+      },
+    ],
+    tecnologies: "TS • Next.js • Tailwind • Stitches • Golang • AWS • Mulesoft",
+  },
+  {
+    position: "Full Stack Developer - @Neoris",
+    web: "https://www.neoris.com/",
+    fromDate: "2021",
+    toDate: "2022",
+    details: [
+      {
+        functionStack: "Frontend",
+        detail:
+          "Desarrollos con NextJs, React Js, Tailwind CSS y Typescript. Además trabajo en una librería de componente con Storybook.",
+      },
+      {
+        functionStack: "Backend",
+        detail:
+          "Utilizando tecnologías como Golang y AWS, además integro infraestructura como código con Terraform y Terragrunt.",
+      },
+    ],
+    tecnologies: "JS • Next.js • React • Node • NoSQL",
+  },
+];

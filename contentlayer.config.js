6ae9e7144a231f60b3e7bda881fbb9465206d7f3
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2023-08-27 18:08:32 -0300

    feat: added category doc type

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,89 +1,97 @@
-import { defineDocumentType, makeSource } from "contentlayer/source-files"
-import rehypeAutolinkHeadings from "rehype-autolink-headings"
-import rehypePrettyCode from "rehype-pretty-code"
-import rehypeSlug from "rehype-slug"
-import remarkGfm from "remark-gfm"
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
 
 /** @type {import('contentlayer/source-files').ComputedFields} */
 const computedFields = {
-    slug: {
-      type: "string",
-      resolve: (doc) => `/${doc._raw.flattenedPath}`,
-    },
-    slugAsParams: {
-      type: "string",
-      resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
-    },
-  }
+  slug: {
+    type: "string",
+    resolve: (doc) => `/${doc._raw.flattenedPath}`,
+  },
+  slugAsParams: {
+    type: "string",
+    resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
+  },
+};
 
 export const Post = defineDocumentType(() => ({
-    name: "Post",
-    filePathPattern: `blog/**/*.mdx`,
-    contentType: "mdx",
-    fields: {
-      title: {
-        type: "string",
-        required: true,
-      },
-      description: {
-        type: "string",
-      },
-      date: {
-        type: "date",
-        required: true,
-      },
-      published: {
-        type: "boolean",
-        default: true,
-      },
-      image: {
-        type: "string",
-        required: true,
-      },
-      author: {
-        type: "string",
-        required: true,
-      },
+  name: "Post",
+  filePathPattern: `blog/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
     },
-    computedFields,
-  }))
-  
-  export default makeSource({
-    contentDirPath: "./content",
-    documentTypes: [ Post ],
-    mdx: {
-      remarkPlugins: [remarkGfm],
-      rehypePlugins: [
-        rehypeSlug,
-        [
-          rehypePrettyCode,
-          {
-            theme: "github-dark",
-            onVisitLine(node) {
-              // Prevent lines from collapsing in `display: grid` mode, and allow empty
-              // lines to be copy/pasted
-              if (node.children.length === 0) {
-                node.children = [{ type: "text", value: " " }]
-              }
-            },
-            onVisitHighlightedLine(node) {
-              node.properties.className.push("line--highlighted")
-            },
-            onVisitHighlightedWord(node) {
-              node.properties.className = ["word--highlighted"]
-            },
+    description: {
+      type: "string",
+    },
+    date: {
+      type: "date",
+      required: true,
+    },
+    published: {
+      type: "boolean",
+      default: true,
+    },
+    image: {
+      type: "string",
+      required: true,
+    },
+    author: {
+      type: "string",
+      required: true,
+    },
+    categories: {
+      type: "list",
+      of: {
+        type: "enum",
+        options: ["Next.js", "React", "Mulesoft"],
+        default: "Sin categoría",
+      },
+      required: true,
+    },
+  },
+  computedFields,
+}));
+
+export default makeSource({
+  contentDirPath: "./content",
+  documentTypes: [Post],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          onVisitLine(node) {
+            // Prevent lines from collapsing in `display: grid` mode, and allow empty
+            // lines to be copy/pasted
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }];
+            }
           },
-        ],
-        [
-          rehypeAutolinkHeadings,
-          {
-            properties: {
-              className: ["subheading-anchor"],
-              ariaLabel: "Link to section",
-            },
+          onVisitHighlightedLine(node) {
+            node.properties.className.push("line--highlighted");
           },
-        ],
+          onVisitHighlightedWord(node) {
+            node.properties.className = ["word--highlighted"];
+          },
+        },
       ],
-    },
-  })
-  
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section",
+          },
+        },
+      ],
+    ],
+  },
+});

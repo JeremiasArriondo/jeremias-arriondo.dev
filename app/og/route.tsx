855a8782e5a540bb986cd332eefd00f2c678923c
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2023-08-29 21:51:31 -0300

    feat: route og images created

---
+++ app/og/route.tsx
@@ -0,0 +1,59 @@
+import { ImageResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+export const runtime = "edge";
+
+const font = fetch(
+  new URL("../../public/fonts/Inter-Bold.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const postTitle = searchParams.get("title");
+
+  const fontData = await font;
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          justifyContent: "center",
+          backgroundImage: "url(https://www.jeremias-arriondo.dev/og-bg.png)",
+        }}
+      >
+        <div
+          style={{
+            marginLeft: 190,
+            marginRight: 190,
+            display: "flex",
+            fontSize: 130,
+            fontFamily: "Inter",
+            letterSpacing: "-0.05em",
+            fontStyle: "normal",
+            color: "white",
+            lineHeight: "120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {postTitle}
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "Inter",
+          data: fontData,
+          style: "normal",
+        },
+      ],
+    }
+  );
+}

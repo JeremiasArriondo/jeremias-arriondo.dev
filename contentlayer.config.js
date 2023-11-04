96ac0ba0cdc551f9488182a6b8f1ac7b2d90f2d2
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2023-11-03 22:29:28 -0300

    feat: new category web

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,70 +1,71 @@
-import { defineDocumentType, makeSource } from "contentlayer/source-files";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypePrettyCode from "rehype-pretty-code";
-import rehypeSlug from "rehype-slug";
-import remarkGfm from "remark-gfm";
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeSlug from 'rehype-slug';
+import remarkGfm from 'remark-gfm';
 
 /** @type {import('contentlayer/source-files').ComputedFields} */
 const computedFields = {
   slug: {
-    type: "string",
-    resolve: (doc) => `/${doc._raw.flattenedPath}`,
+    type: 'string',
+    resolve: (doc) => `/${doc._raw.flattenedPath}`
   },
   slugAsParams: {
-    type: "string",
-    resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
-  },
+    type: 'string',
+    resolve: (doc) => doc._raw.flattenedPath.split('/').slice(1).join('/')
+  }
 };
 
 export const Post = defineDocumentType(() => ({
-  name: "Post",
+  name: 'Post',
   filePathPattern: `blog/**/*.mdx`,
-  contentType: "mdx",
+  contentType: 'mdx',
   fields: {
     title: {
-      type: "string",
-      required: true,
+      type: 'string',
+      required: true
     },
     description: {
-      type: "string",
+      type: 'string'
     },
     date: {
-      type: "date",
-      required: true,
+      type: 'date',
+      required: true
     },
     published: {
-      type: "boolean",
-      default: true,
+      type: 'boolean',
+      default: true
     },
     image: {
-      type: "string",
-      required: true,
+      type: 'string',
+      required: true
     },
     author: {
-      type: "string",
-      required: true,
+      type: 'string',
+      required: true
     },
     categories: {
-      type: "list",
+      type: 'list',
       of: {
-        type: "enum",
+        type: 'enum',
         options: [
-          "Next.js",
-          "React",
-          "Mulesoft",
-          "Programming",
-          "Sin categoría",
+          'Next.js',
+          'React',
+          'Mulesoft',
+          'Programming',
+          'Web',
+          'Sin categoría'
         ],
-        default: "Sin categoría",
+        default: 'Sin categoría'
       },
-      required: true,
-    },
+      required: true
+    }
   },
-  computedFields,
+  computedFields
 }));
 
 export default makeSource({
-  contentDirPath: "./content",
+  contentDirPath: './content',
   documentTypes: [Post],
   mdx: {
     remarkPlugins: [remarkGfm],
@@ -73,32 +74,32 @@ export default makeSource({
       [
         rehypePrettyCode,
         {
-          theme: "github-dark",
+          theme: 'github-dark',
           onVisitLine(node) {
             // Prevent lines from collapsing in `display: grid` mode, and allow empty
             // lines to be copy/pasted
             if (node.children.length === 0) {
-              node.children = [{ type: "text", value: " " }];
+              node.children = [{ type: 'text', value: ' ' }];
             }
           },
           onVisitHighlightedLine(node) {
-            node.properties.className.push("line--highlighted");
+            node.properties.className.push('line--highlighted');
           },
           onVisitHighlightedWord(node) {
-            node.properties.className = ["word--highlighted"];
-          },
-        },
+            node.properties.className = ['word--highlighted'];
+          }
+        }
       ],
       [
         rehypeAutolinkHeadings,
         {
           properties: {
-            className: ["anchor"],
-            "data-mdx-heading": "",
-            ariaLabel: "Link to section",
-          },
-        },
-      ],
-    ],
-  },
+            className: ['anchor'],
+            'data-mdx-heading': '',
+            ariaLabel: 'Link to section'
+          }
+        }
+      ]
+    ]
+  }
 });

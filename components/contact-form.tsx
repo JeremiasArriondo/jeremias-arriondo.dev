66b379f1fa8788da54a7efffe4e9ecb457026625
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2025-03-06 13:38:57 -0300

    feat: form contact created

---
+++ components/contact-form.tsx
@@ -0,0 +1,113 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const contactFormSchema = z.object({
+  name: z.string().min(2, { message: "Por favor ingresa tu nombre" }),
+  email: z
+    .string()
+    .min(2, {
+      message: "El email debe estar completo",
+    })
+    .email("Ingrese un email válido"),
+  message: z.string().min(10, {
+    message: "Asegúrese de que su mensaje tenga al menos 10 caracteres.",
+  }),
+});
+
+export const ContactForm = () => {
+  const form = useForm<z.infer<typeof contactFormSchema>>({
+    resolver: zodResolver(contactFormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof contactFormSchema>) => {
+    try {
+      const res = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+
+      console.log(res);
+
+      if (!res.ok) throw new Error("Error al enviar el mensaje");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        className="grid grid-cols-3 items-center p-4 lg:p-6"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <div className="col-span-3 flex flex-col gap-4 lg:col-span-3 lg:gap-6">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nombre</FormLabel>
+                <FormControl>
+                  <Input placeholder="John Doe" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="john@example.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Mensaje</FormLabel>
+                <FormControl>
+                  <Textarea {...field} placeholder="Tu mensaje" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button disabled={isLoading}>
+            {isLoading ? "Enviando....." : "Enviar"}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};

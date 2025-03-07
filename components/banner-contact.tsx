d2e43ae2548fa32b4fac835eba73a965b54efc0a
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2025-03-07 15:50:07 -0300

    styles: shadow added bannar contact

--- components/banner-contact.tsx
+++ components/banner-contact.tsx
@@ -3,7 +3,7 @@ import { IconRocket } from "./Icons/IconRocket";
 
 export function BannerContact() {
   return (
-    <div className="p-4 rounded-xl bg-gray-50 dark:bg-customGray-dark flex flex-col gap-4">
+    <div className="p-4 rounded-xl bg-gray-50 dark:bg-customGray-dark flex flex-col gap-4 shadow-xl shadow-violet-400/20 dark:border dark:border-gray-800">
       <div className="flex items-center gap-2">
         <IconRocket className="animate-bounce" />
         <h3>Trabajemos juntos!</h3>
@@ -14,9 +14,9 @@ export function BannerContact() {
       </p>
       <Link
         href="/contact"
-        className="self-start inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+        className="self-start inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-violetPrimary/30 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
       >
-        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+        <span className="relative px-5 py-1.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
           Contactame
         </span>
       </Link>
